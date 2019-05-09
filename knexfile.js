309eb84d7dad9dbb7d36a33ddbd0189a1eb9a622
ohheyimjen <jendority@gmail.com>
2019-05-09 14:45:54 -0700

    created dishes table

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,15 @@ module.exports = {
     }
   },
   migrations: {
-    directory: ''
+    directory: './data/migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  },
+  pool: {
+    afterCreate: (conn,done) => {
+      conn.run(('PRAGMA foreign_keys = ON', done));
+    }
   }
   
 };
