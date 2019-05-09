437387a186011cc6f178516e8bf2e7f3a6cec833
ohheyimjen <jendority@gmail.com>
2019-05-09 15:22:47 -0700

    created shopping list table

--- data/migrations/20190509150701_createShoppingListTable.js
+++ data/migrations/20190509150701_createShoppingListTable.js
@@ -1,8 +1,31 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('shopping_list', tbl => {
+    tbl.integer('ingredient_id');
+
+    tbl
+      .foreign('ingredient_id')
+      .reference('id')
+      .unsigned()
+      .inTable('ingredients')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+
+    tbl.integer('recipe_id')
+      .foreign('recipe_id')
+      .references('id')
+      .unsigned()
+      .inTable('recipes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+
+    tbl.float('amount')
+      .unsigned()
+      .notNullable()
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('shopping_list');
 };
